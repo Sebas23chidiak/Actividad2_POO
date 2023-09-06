e3bc536662a1ef528ca2728f4faa97c7dce788bd
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-06 02:30:06 -0500

    Create EJERCICIO_R11_C4.py

---
+++ EJERCICIO_R11_C4.py
@@ -0,0 +1,8 @@
+# Ingresar por el usuario tres números enteros diferentes
+numero1 = int(input("Ingrese el primer número entero: "))
+numero2 = int(input("Ingrese el segundo número entero: "))
+numero3 = int(input("Ingrese el tercer número entero: "))
+# Encontrar el mayor de los tres números usando la función max()
+mayor = max(numero1, numero2, numero3)
+# Se muestra el resultado
+print(f"El número mayor de los tres es: {mayor}")
